9bed49af5cfe407414dab47f7b81e13147404ced
R-unic <roleyspersonal@gmail.com>
2024-03-20 13:38:10 -0400

    feat: make hovering look/feel better for DeckHand UI component

--- src/client/components/ui/deck-hand.ts
+++ src/client/components/ui/deck-hand.ts
@@ -18,8 +18,10 @@ export class DeckHand extends DestroyableComponent<{}, Frame & { UIListLayout: U
   public onStart(): void {
     const screen = this.instance.FindFirstAncestorOfClass("ScreenGui")!;
     const mouse = Player.GetMouse();
-    for (const card of this.getCards())
-      new Instance("UIScale", card);
+    for (const card of this.getCards()) {
+      const scale = new Instance("UIScale", card);
+      scale.Scale = 1;
+    }
 
     this.janitor.Add(this.instance);
     this.janitor.Add(mouse.Move.Connect(() => {
@@ -28,22 +30,30 @@ export class DeckHand extends DestroyableComponent<{}, Frame & { UIListLayout: U
       const frameSize = this.instance.AbsoluteSize;
       const frameDimensionsX = new Range(framePosition.X, framePosition.X + frameSize.X);
       const frameDimensionsY = new Range(framePosition.Y, framePosition.Y + frameSize.Y);
-      if (frameDimensionsX.numberIsWithin(X) && frameDimensionsY.numberIsWithin(Y)) {
-        for (const card of this.getCards())
-          task.spawn(() => {
-            const cardDistanceFromMouse = new Vector2(X, Y).sub(card.AbsolutePosition).Magnitude;
-            const scaleIncrement = math.clamp(1 - (cardDistanceFromMouse / (screen.AbsoluteSize.Magnitude - frameSize.Magnitude) * 2), 0, 1);
-            card.UIScale.Scale = 1 + scaleIncrement;
-          });
-        return;
-      } else
+      if (!(frameDimensionsX.numberIsWithin(X) && frameDimensionsY.numberIsWithin(Y))) {
         for (const card of this.getCards())
           card.UIScale.Scale = 1;
+        return;
+      }
+
+      for (const card of this.getCards())
+        task.spawn(() => {
+          const cardPosition = card.AbsolutePosition;
+          const cardSize = card.AbsoluteSize;
+          const cardDistanceFromMouseVector = new Vector2(X, Y).sub(cardPosition.add(new Vector2(cardSize.X / 2, 0)));
+          const cardDistanceFromMouse = (cardDistanceFromMouseVector.sub(new Vector2(0, cardDistanceFromMouseVector.Y))).Magnitude;
+          const scaleIncrement = math.clamp(1 - (cardDistanceFromMouse / (screen.AbsoluteSize.Magnitude - frameSize.Magnitude) * 2), 0, 1);
+          card.UIScale.Scale = 1 + this.modifyScaleIncrement(scaleIncrement);
+        });
     }));
   }
 
   private getCards(): CardFrame[] {
     return this.instance.GetChildren()
-      .filter((i): i is CardFrame => i.IsA("ImageLabel") || i.IsA("ImageButton"));
+      .filter((i): i is CardFrame => i.IsA("ImageButton"));
+  }
+
+  private modifyScaleIncrement(scaleIncrement: number): number {
+    return scaleIncrement ** 3 * 0.75;
   }
 }

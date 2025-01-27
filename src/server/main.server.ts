91b5dc1e6c552ef214fe6ad4cea1255321dfb316
R-unic <roleyspersonal@gmail.com>
2025-01-27 15:46:51 -0500

    feat: setup asset syncing

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,12 +1,3 @@
-import { OnMessage } from "./decorators";
 import { MessageEmitter } from "shared/structs/messages/emitter";
-import { MessageKind } from "shared/structs/messages/kind";
-import type { TestMessageData } from "shared/structs/messages/data";
 
 MessageEmitter.initialize();
-export class Server {
-  @OnMessage(MessageKind.TEST)
-  public onTestMessage(player: Player, data: TestMessageData): void {
-    print(player, "sent", data, "via TEST message");
-  }
-}

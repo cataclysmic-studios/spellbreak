7887e2f2dbf9db9b41478fbc406645b8b60b1950
R-unic <roleyspersonal@gmail.com>
2025-02-05 19:14:39 -0500

    chore: new assets typings

--- types/ReplicatedStorage.d.ts
+++ types/ReplicatedStorage.d.ts
@@ -1,5 +1,32 @@
 interface ReplicatedStorage extends Instance {
   assets: Folder & {
+    animations: Folder & {
+      battle: Folder & {
+        circle: Folder & {
+          idle: Animation;
+          fighterAdded: Animation;
+          fighterRemoved: Animation;
+        };
+      }
+    };
+    battle: Folder & {
+      circle: BattleCircleModel;
+      selectionTarget: Model;
+      fighterSigil: MeshPart;
+      pip: Part;
+      powerPip: Part;
+      shadowPip: Part;
+      pointer: Part;
+      pipPositions: Model & {
+        "1": Part;
+        "2": Part;
+        "3": Part;
+        "4": Part;
+        "5": Part;
+        "6": Part;
+        "7": Part;
+      };
+    };
     characters: Folder & {
       roslyn: CharacterModel;
     };
--- types/game.d.ts
+++ types/game.d.ts
@@ -1,5 +1,20 @@
-interface CharacterModel extends Model {
-  Collider: Part;
+interface BattleCirclePositions extends Folder {
+  "1": Part;
+  "2": Part;
+  "3": Part;
+  "4": Part;
+}
+
+interface BattleCircleModel extends Model, WithAnimationController {
+  opponentPositions: BattleCirclePositions;
+  teamPositions: BattleCirclePositions;
+}
+
+interface CharacterModel extends Model, WithAnimationController {
+  collider: Part;
+}
+
+interface WithAnimationController {
   AnimationController: AnimationController & {
     Animator: Animator;
   };
